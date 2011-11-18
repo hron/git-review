12ec07e8ad486243082980f86eee1f0bd315164d
Aleksei Gusev <aleksei.gusev@gmail.com>
2011-11-18 15:08:42 +0300

    added pushing notes to origin.

--- lib/git_review_branch/cli.rb
+++ lib/git_review_branch/cli.rb
@@ -5,9 +5,6 @@ module GitReviewBranch
   class CLI < Thor
     desc "review", "Add note with Reviewed-By: for each commit in current branch."
     def review
-      # git = Git.open(".", :log => Logger.new(STDOUT))
-      git = Git.open(".")
-
       raise "Current branch is master. Exiting..." if git.current_branch == 'master'
 
       commits_to_review = git.log.between("master", git.current_branch)
@@ -20,9 +17,17 @@ module GitReviewBranch
         git.lib.add_note(commit, note)
       end
 
+      system "git push origin refs/notes/*"
       system "git log master..#{git.current_branch}"
       puts
       say "#{commits_to_review.size} commits are marked as reviewed by you.", :green
+    end
+
+    private
+
+    def git
+      # @git ||= Git.open(".", :log => Logger.new(STDOUT))
+      @git ||= Git.open(".")
     end
   end
 end
